198ba3120c9b998ae9c39ff8e468be45ab3b5698
Mittsy Tidwell <mtidwell506@gmail.com>
2017-05-04 16:59:34 -0500

    working with strings

---
+++ Java24/src/com/java24hours/StringCase.java
@@ -0,0 +1,9 @@
+package com.java24hours;
+
+class StringCase {
+	public static void main (String[] args) {
+		String fin = "Ian Ziering";
+		System.out.println(fin.toUpperCase());
+		System.out.println(fin.toLowerCase());
+	}
+}
---
+++ Java24/src/com/java24hours/StringLength.java
@@ -0,0 +1,8 @@
+package com.java24hours;
+
+public class StringLength {
+	public static void main(String[] args) {
+		String cinematographer = "Ben Demaree";
+		System.out.println(cinematographer.length());
+	}
+}
