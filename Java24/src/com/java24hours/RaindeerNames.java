cba979c09c24474de33bd4909a4ff12f68e9ba81
Mittsy Tidwell <mtidwell506@gmail.com>
2017-05-11 17:21:06 -0500

    working with arrays

---
+++ Java24/src/com/java24hours/RaindeerNames.java
@@ -0,0 +1,16 @@
+package com.java24hours;
+
+public class RaindeerNames {
+	public static void main (String[] args) {
+//		If you state how many elements are going to be in the array, you can not add or remove more elements
+		String [] reindeerNames = new String [8];
+		reindeerNames[0] = "Dasher";
+		reindeerNames[1] = "Dancer";
+		reindeerNames[2] = "Pracner";
+		reindeerNames[3] = "vixon";
+		reindeerNames[4] = "Comet";
+		reindeerNames[5] = "Cupid";
+		reindeerNames[6] = "Donder";
+		reindeerNames[7] = "Blitzen";
+	}
+}
