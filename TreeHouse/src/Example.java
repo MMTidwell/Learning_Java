af43d7fd49708594ca29c3fa784b94f0719f13c9
Mittsy Tidwell <mtidwell506@gmail.com>
2017-08-31 15:32:59 -0500

    data structures

--- TreeHouse/src/Example.java
+++ TreeHouse/src/Example.java
@@ -1,14 +1,48 @@
+import java.util.Arrays;
 import java.util.Date;
+
 import com.teamtreehouse.Treet;
+import com.teamtreehouse.Treets;
+
 
 public class Example {
 	public static void main(String[] args) {
-		Treet treet = new Treet(
-				"craigsdennis",
-				"Want to be famous? Simply tweet about Java and use the hashtag #treet. Ill use your tweet in a new @treehouse course about data structures.",
-				new Date(1421849732000L)
-		);
-		System.out.printf("This is a new Treet: %s\n", treet);
+		// creates a new instance of treet
+//		Treet treet = new Treet(
+//				"craigsdennis",
+//				"Want to be famous? Simply tweet about Java and use the hashtag #treet. Ill use your tweet in a new @treehouse course about data structures.",
+//				new Date(1421849732000L)
+//		);
+//		
+//		Treet secondTreet = new Treet(
+//				"journeytocode",
+//				"@treehouse makes learning Java soooooo fun! #treet",
+//				new Date(1421878767000L)
+//		);
+//		
+//		// prints
+//		System.out.printf("This is a new Treet: %s\n", treet);
+//		System.out.println("The words are:");
+//		
+//		// prints each word in the array
+//		for (String word: treet.getWords()) {
+//			System.out.println(word);
+//		}
+//		
+//		// creates treets array
+//		Treet[] treets = {treet, secondTreet};
+//		// sorts treets
+//		Arrays.sort(treets);
+//		
+//		// prints out each treet based on the array
+//		for (Treet exampleTreet : treets) {
+//			System.out.println(exampleTreet);
+//		}
+//		Treets.save(treets);
+		Treet[] reloadedTreets = Treets.load();
+		for (Treet reloaded : reloadedTreets) {
+			System.out.println(reloaded);
+		}
 	}
 }
 
