82c67fff70dbcb98092edcd3deff82f97751e896
Mittsy Tidwell <mtidwell506@gmail.com>
2017-09-05 16:59:17 -0500

    hangman round 2

---
+++ TreeHouse/src/com/treehouse/Hangman.java
@@ -0,0 +1,9 @@
+package com.treehouse;
+
+// Hangman Class is the main java file
+public class Hangman {
+	public static void main (String[] args) {
+		// create instance of Game class, passing in the answer as the parameter
+		Game game = new Game("treehouse");
+	}
+}
---
+++ TreeHouse/src/com/treehouse/Prompter.java
@@ -0,0 +1,6 @@
+package com.treehouse;
+
+// Prompter Class is used for all the I/O (input/output)
+public class Prompter {
+	
+}
